9016719c1eb2842c46eb6206bcda2247b53c0d0a
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-08 19:15:45 -0300

    paginacao

--- frontend/src/Pages/Products/List/index.jsx
+++ frontend/src/Pages/Products/List/index.jsx
@@ -1,16 +1,72 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Button } from "react-bootstrap";
 import { ModalCreateProduct } from "../../../Components/Modal/ModalCreateProduct";
-import { useGlobalContext } from "../../../Context/GlobalContext/context";
+import { ProductCard } from "../../../Components/Product";
 import { setLoading } from "../../../Context/GlobalContext/actions";
+import { useGlobalContext } from "../../../Context/GlobalContext/context";
+import { env } from "../../../Environment";
 
 function App() {
 
   const { dispatch } = useGlobalContext();
 
+  const isMounted = useRef(true);
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [products, setProducts] = useState({
+    result: [],
+    total: 0,
+    page: 1,
+    per_page: 5,
+    total_pages: 0
+  });
+
+
+  const getProducts = async () => {
+    setLoading(dispatch);
+
+    const body = new FormData();
+    body.append('page', products.page)
+    body.append('limit', products.per_page)
+
+    const res = await fetch(env.api.url.dev + '?page=products&method=list',
+      {
+        method: 'post',
+        body
+      })
+    const json = await res.json();
+    setProducts({ ...json });
+    setLoading(dispatch);
+  }
+
+  const nextPage = () => {
+    products.page = products.page + 1
+    setProducts(products)
+    getProducts()
+  }
+
+  const previusPage = () => {
+    products.page = products.page - 1
+    setProducts(products)
+    getProducts()
+  }
+
+  const goToPage = (page) => {
+    products.page = page;
+    setProducts(products)
+    getProducts()
+  }
+
+  useEffect(() => {
+    if (isMounted.current) {
+      getProducts();      
+    }
+
+    return () => isMounted.current = false;
+  }, [getProducts])
 
   return (
     <div style={{ padding: '10px 50px' }}>
@@ -32,16 +88,9 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td style={{ textAlign: "right" }}>
-              <Button variant="danger" size="sm" className="mx-2">Delete</Button>
-            </td>
-          </tr>
+          {!!products.result && products.result.map(p => {
+            return <ProductCard key={p.id} product={p} getProducts={getProducts} />
+          })}
         </tbody>
       </table>
 
@@ -49,19 +98,24 @@ function App() {
         show={show}
         handleClose={handleClose}
         handleShow={handleShow}
+        getProducts={getProducts}
       />
 
 
       <nav aria-label="Page navigation example">
         <ul className="pagination justify-content-center">
-          <li className="page-item disabled">
-            <a className="page-link">Previous</a>
+          <li className={products.page === 1 ? "page-item disabled" : "page-item"}>
+            <a href="#/" className="page-link" onClick={() => previusPage()}>Previous</a>
           </li>
-          <li className="page-item"><a className="page-link" href="#">1</a></li>
-          <li className="page-item"><a className="page-link" href="#">2</a></li>
-          <li className="page-item"><a className="page-link" href="#">3</a></li>
-          <li className="page-item">
-            <a className="page-link" href="#">Next</a>
+          {products.total_pages !== 0 && [...Array(products.total_pages)].map((v, i) => {
+            return (
+              <li key={i} className={products.page === i + 1? "page-item active" : "page-item"} onClick={() => goToPage(i + 1)}>
+                <a  href="#/" className="page-link">{i + 1}</a>
+              </li>
+            )
+          })}
+          <li className={products.page === products.total_pages ? "page-item disabled" : "page-item"}>
+            <a  href="#/" className="page-link" onClick={() => nextPage()}>Next</a>
           </li>
         </ul>
       </nav>

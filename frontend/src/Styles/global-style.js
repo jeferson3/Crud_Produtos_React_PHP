c3bd1a7d03e8561b6c8728ceb4cf2a30060e50a5
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-08 11:15:39 -0300

    estilos

---
+++ frontend/src/Styles/global-style.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+        box-shadow: none !important;
+    }
+
+    .required::after {
+        content: '*';
+        color: red;
+    }
+
+`

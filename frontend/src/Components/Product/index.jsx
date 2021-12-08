45b99d7d15040116ef3b9bd247cb55820a26bff8
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-08 20:15:32 -0300

    finalizando

--- frontend/src/Components/Product/index.jsx
+++ frontend/src/Components/Product/index.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Button, Modal } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Button, Col, FloatingLabel, Form, Modal, Row } from "react-bootstrap"
 import { setLoading, setToast } from "../../Context/GlobalContext/actions";
 import { useGlobalContext } from "../../Context/GlobalContext/context";
 import { env } from "../../Environment";
@@ -8,13 +8,29 @@ export const ProductCard = ({ product, getProducts }) => {
 
     const { state: { toast }, dispatch } = useGlobalContext();
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [showFormDelete, setShowFormDelete] = useState(false);
+    const handleCloseFormDelete = () => setShowFormDelete(false);
+    const handleshowFormDeleteFormDelete = () => setShowFormDelete(true);
 
-    const handleSubmitForm = async () => {
+    const [showFormEdit, setShowFormEdit] = useState(false);
+    const handleCloseFormEdit = () => setShowFormEdit(false);
+    const handleshowFormEditFormEdit = () => setShowFormEdit(true);
 
-        handleClose();
+    const [inputName, setInputName] = useState(product.name);
+    const [inputPrice, setInputPrice] = useState(product.price[0]);
+    const [inputCategoryId, setInputCategoryId] = useState(product.category_id[0]);
+    const [inputPurchaseTime, setInputPurchaseTime] = useState(product.purchase_time[0]);
+    const [inputPerishable, setInputPerishable] = useState(product.is_perishable ? 1 : 2);
+
+
+    const [categories, setCategories] = useState([]);
+
+    const disableButtonSave = (!inputName || !inputPrice || !inputCategoryId ||
+        !inputPurchaseTime || (inputPerishable == null || inputPerishable == undefined));
+
+    const handleSubmitFormDelete = async () => {
+
+        handleCloseFormDelete();
         setLoading(dispatch);
 
         const body = new FormData();
@@ -34,6 +50,53 @@ export const ProductCard = ({ product, getProducts }) => {
 
         getProducts();
     }
+
+    const handleSubmitFormUpdate = async () => {
+
+        console.log(inputCategoryId);
+
+        handleCloseFormEdit();
+
+        setLoading(dispatch);
+
+        const body = new FormData();
+        body.append('id', product.id);
+        body.append('name', inputName);
+        body.append('price', inputPrice);
+        body.append('category_id', inputCategoryId);
+        body.append('purchase_time', inputPurchaseTime);
+        body.append('is_perishable', inputPerishable);
+
+        const res = await fetch(env.api.url.dev + '?page=products&method=update',
+            {
+                method: 'post',
+                body
+            })
+
+        const json = await res.json();
+
+        setLoading(dispatch);
+
+        if (json.status) setToast(dispatch, { ...toast, status: true, type: 'primary', msg: json.message })
+        else setToast(dispatch, { ...toast, status: true, type: 'danger', msg: json.message })
+
+        getProducts();
+    }
+
+    useEffect(() => {
+
+        async function getCategories() {
+            const res = await fetch(env.api.url.dev + '?page=categories&method=list',
+                {
+                    method: 'post'
+                })
+            const resJson = await res.json();
+            setCategories(resJson);
+        }
+        getCategories();
+    }, [])
+
+
     return (
         <>
             <tr>
@@ -44,16 +107,26 @@ export const ProductCard = ({ product, getProducts }) => {
                 <td>{product.purchase_time[1]}</td>
                 <td style={{ textAlign: "right" }}>
                     <Button
+                        variant="success"
+                        size="sm"
+                        className="mx-2"
+                        onClick={handleshowFormEditFormEdit}
+                    >
+                        Edit
+                    </Button>
+                    <Button
                         variant="danger"
                         size="sm"
                         className="mx-2"
-                        onClick={handleShow}
-                    >Delete</Button>
+                        onClick={handleshowFormDeleteFormDelete}
+                    >
+                        Delete
+                    </Button>
                 </td>
             </tr>
             <Modal
-                show={show}
-                onHide={() => handleClose()}
+                show={showFormDelete}
+                onHide={() => handleCloseFormDelete()}
                 size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
@@ -65,14 +138,91 @@ export const ProductCard = ({ product, getProducts }) => {
                     <h5>Confirme a exclusão do produto #{product.name}</h5>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" size="sm" onClick={() => handleClose()}>
+                    <Button variant="secondary" size="sm" onClick={() => handleCloseFormDelete()}>
                         Fechar
                     </Button>
                     <Button
                         variant="primary"
                         size="sm"
-                        onClick={() => handleSubmitForm()}>
+                        onClick={() => handleSubmitFormDelete()}>
                         Deletar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal
+                show={showFormEdit}
+                onHide={() => handleCloseFormEdit()}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title className="text-uppercase">Editar Produto #{product.id}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Row>
+                            <Col sm={12} md={12} lg={12}>
+                                <Form.Group className="pb-2">
+                                    <FloatingLabel
+                                        controlId="name"
+                                        label="Nome"
+                                    >
+                                        <Form.Control placeholder="nome" value={inputName} onChange={e => setInputName(e.target.value)} />
+                                    </FloatingLabel>
+                                </Form.Group>
+                            </Col>
+                            <Col sm={12} md={12} lg={12}>
+                                <Form.Group className="pb-2">
+                                    <FloatingLabel controlId="category_id" label="Categoria">
+                                        <Form.Select onChange={e => setInputCategoryId(e.target.value)} defaultValue={inputCategoryId}>
+                                            <option>{"Selecione uma cetegoria"}</option>
+                                            {!!categories && categories.map(c => <option key={c.id} value={c.id}>{c.name}</option>)}
+                                        </Form.Select>
+                                    </FloatingLabel>
+                                </Form.Group>
+                            </Col>
+                            <Col sm={12} md={12} lg={12}>
+                                <Form.Group className="pb-2">
+                                    <FloatingLabel
+                                        controlId="price"
+                                        label="Preço"
+                                    >
+                                        <Form.Control type="number" value={inputPrice} onChange={e => setInputPrice(e.target.value)} placeholder="preço" />
+                                    </FloatingLabel>
+                                </Form.Group>
+                            </Col>
+                            <Col sm={12} md={12} lg={12}>
+                                <Form.Group className="pb-2">
+                                    <FloatingLabel
+                                        controlId="purchase_time"
+                                        label="Data Compra"
+                                    >
+                                        <Form.Control type="datetime-local" value={inputPurchaseTime} onChange={e => setInputPurchaseTime(e.target.value)} />
+                                    </FloatingLabel>
+                                </Form.Group>
+                            </Col>
+                            <Col sm={12} md={12} lg={12}>
+                                <Form.Group className="pb-2">
+                                    <Form.Label>Perecível: </Form.Label>
+                                    <Form.Check onClick={() => setInputPerishable(1)} type="radio" id="perishable-s" name="perishable" label="Sim" defaultChecked={inputPerishable} />
+                                    <Form.Check onClick={() => setInputPerishable(2)} type="radio" id="perishable-n" name="perishable" label="Não" defaultChecked={!inputPerishable} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" size="sm" onClick={() => handleCloseFormEdit()}>
+                        Fechar
+                    </Button>
+                    <Button
+                        variant="primary"
+                        size="sm"
+                        disabled={disableButtonSave}
+                        onClick={() => handleSubmitFormUpdate()}>
+                        Salvar
                     </Button>
                 </Modal.Footer>
             </Modal>
